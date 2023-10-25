f69da6a03168067b1cda944a176126f4e38dfdd5
Nikita Stepochkin <n.stepochkin@headsandhands.ru>
2023-10-25 10:36:47 +0300

    feat: base tracking functionality

--- background.js
+++ background.js
@@ -1,15 +1,75 @@
+function formatRedmineDate(jiraStartDate) {
+    let dateComponents = jiraStartDate.substring(0, 8).split('.');
+    let day = dateComponents[0];
+    let month = dateComponents[1];
+
+    let year;
+    if (dateComponents[2].length === 2) {
+        year = "20" + dateComponents[2];
+    } else {
+        year = dateComponents[2];
+    }
+
+    return `${year}-${month}-${day}`;
+}
+
+function sendSuccessNotification(jiraTimeLogged, formattedDate, jiraIssueName) {
+    chrome.notifications.create(
+        "Автотрекер Redmine",
+        {
+            type: "basic",
+            iconUrl: "icons/icon128.png",
+            title: 'Автотрекер Redmine',
+            message: `Внесено ${jiraTimeLogged} часов за ${formattedDate} по задаче ${jiraIssueName}`
+        }
+    );
+}
+
+function trackTime(jiraTimeTrackRequest, extensionParams) {
+    let jiraUrl = extensionParams.jiraUrl;
+    let jiraApiKey = extensionParams.jiraApiKey;
+    let redmineUrl = extensionParams.redmineUrl;
+    let redmineApiKey = extensionParams.redmineApiKey;
+    let redmineIssueId = extensionParams.redmineIssueId;
+
+    let jiraIssueId = jiraTimeTrackRequest.requestBody.formData.id;
+    let jiraTimeLogged = jiraTimeTrackRequest.requestBody.formData.timeLogged[0];
+    let jiraStartDate = jiraTimeTrackRequest.requestBody.formData.startDate[0];
+    let formattedDate = formatRedmineDate(jiraStartDate);
+
+    fetch(jiraUrl + "/rest/api/2/issue/" + jiraIssueId, {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + jiraApiKey,
+        },
+    }).then(response => {
+        response.json().then((json) => {
+            let jiraIssueName = json.key;
+            let jiraIssueUrl = jiraUrl + '/browse/' + jiraIssueName;
+
+            fetch(redmineUrl + '/time_entries.json', {
+                method: 'POST',
+                body: '{"issue_id":' + redmineIssueId + ',"time_entry":{"spent_on":"' + formattedDate + '","hours":"' + jiraTimeLogged + '","comments":"' + jiraIssueUrl + '","activity_id":9}}',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Redmine-API-Key': redmineApiKey,
+                },
+            }).then(response => response.json()
+                .then(() => sendSuccessNotification(jiraTimeLogged, formattedDate, jiraIssueName))
+            );
+        });
+    });
+}
+
 chrome.webRequest.onBeforeRequest.addListener(
-    function (details) {
-        // Extract parameters from chrome.storage.local
-        chrome.storage.local.get(['jiraUrl', 'jiraApiKey', 'redmineUrl', 'redmineApiKey', 'redmineIssueId'], function (data) {
-            const jiraUrl = data.jiraUrl;
-            const jiraApiKey = data.jiraApiKey;
-            const redmineUrl = data.redmineUrl;
-            const redmineApiKey = data.redmineApiKey;
-            const redmineIssueId = data.redmineIssueId;
-
-
+    function (jiraRequest, tab) {
+        chrome.storage.local.get(['jiraUrl', 'jiraApiKey', 'redmineUrl', 'redmineApiKey', 'redmineIssueId', 'isTrackingEnabled'], function (extensionParams) {
+            let isTrackingEnabled = extensionParams.isTrackingEnabled;
+            if (isTrackingEnabled) {
+                trackTime(jiraRequest, extensionParams);
+            }
         });
     },
-    {urls: ["<all_urls>"]}
+    {urls: ["https://jira.app.local/secure/CreateWorklog.jspa"]},
+    ["requestBody"]
 );
